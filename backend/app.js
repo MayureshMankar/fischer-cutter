5ef44fadd0e89445d2f5d92017fd1bf48a9b7e44
MayureshMankar <92122138+MayureshMankar@users.noreply.github.com>
2024-12-09 21:20:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 
